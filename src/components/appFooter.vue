cc2e5d3b7e06c533214b732e120f7474430e426a
Zackaria S <79061155+Zackaria-Slimane@users.noreply.github.com>
2021-12-31 09:18:08 +0100

    Update appFooter.vue

--- src/components/appFooter.vue
+++ src/components/appFooter.vue
@@ -1,12 +1,7 @@
 <template>
 	<div>
-		<p class="text-center mt-12">
-			Built with
-			<a href="https://v3.vuejs.org/" class="text-mainblue">Vue JS</a>
-			and
-			<a href="https://tailwindcss.com/" class="text-mainblue">Tailwind CSS</a>
-		</p>
-		<p class="text-center text-mainblue">
+
+		<p class="text-center text-mainblue mt-10">
 			<a href="https://github.com/Zackaria-Slimane/Vue3-getanime">Github repo</a>
 		</p>
 	</div>
