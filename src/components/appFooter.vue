3855040081d6e7a810d17588f12ecc87e15550a0
Zakaria Slimane <zackary@Zackarys-MBA.local>
2022-01-02 19:13:15 +0100

    changed footer

--- src/components/appFooter.vue
+++ src/components/appFooter.vue
@@ -1,9 +1,15 @@
 <template>
-	<div>
-		<p class="text-center text-honey mt-10">
-			<a href="https://github.com/Zackaria-Slimane/Vue3-getanime">Github repo</a>
-		</p>
-	</div>
+	<footer class="flex flex-row justify-center mt-28">
+		<a
+			class="text-center text-honey mt-3"
+			href="https://github.com/Zackaria-Slimane/Vue3-getanime"
+		>
+			<img src="https://img.icons8.com/plasticine/38/000000/github-squared.png" />
+		</a>
+		<a class="text-center text-honey mt-3" href="https://twitter.com/gitignorer">
+			<img src="https://img.icons8.com/plasticine/38/000000/twitter--v1.png" />
+		</a>
+	</footer>
 </template>
 <script>
 	export default {
